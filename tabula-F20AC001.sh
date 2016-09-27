ff8cf5584fff6a4a148580decb096e65a053ee16
sanksg <sankalp.gaur@gmail.com>
2016-09-27 08:17:09 +0530

    Added ipython notebooks as well

--- tabula-F20AC001.sh
+++
@@ -1,3 +0,0 @@
-java -jar tabula-java.jar  -a 314.13,57.354,497.241,824.526 -p 1 "$1" 
-java -jar tabula-java.jar  -a 72.087,58.406,484.613,823.474 -p 2 "$1" 
-java -jar tabula-java.jar  -a 71.034,56.301,354.12,828.735 -p 8 "$1" 
